3c584a5721220113fa278ea96aa9c38b1a2e88f5
George Mission <gkmission@outlook.com>
2026-03-17 22:45:09 +0300

    Add project files.

---
+++ HotelBookingBlazor/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,8 @@
+namespace HotelBookingBlazor.Components.Account
+{
+    public class PasskeyInputModel
+    {
+        public string? CredentialJson { get; set; }
+        public string? Error { get; set; }
+    }
+}
---
+++ HotelBookingBlazor/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,8 @@
+namespace HotelBookingBlazor.Components.Account
+{
+    public enum PasskeyOperation
+    {
+        Create = 0,
+        Request = 1,
+    }
+}
