9c0f4f470a0045533dd5594d441d4e730bf635d4
George Mission <gkmission@outlook.com>
2026-03-20 09:12:50 +0300

    Finished importing the template and creating reusable components

--- HotelBookingBlazor/Data/ApplicationUser.cs
+++ HotelBookingBlazor/Data/ApplicationUser.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace HotelBookingBlazor.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+       
     }
 
 }
