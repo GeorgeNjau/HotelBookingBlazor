a226f3287cfd018ffeba6ed5eb74cba20b1cbc23
George Mission <gkmission@outlook.com>
2026-03-20 09:58:47 +0300

    Modified application user entity to add contact property

--- HotelBookingBlazor/Data/ApplicationUser.cs
+++ HotelBookingBlazor/Data/ApplicationUser.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 
 namespace HotelBookingBlazor.Data
@@ -6,13 +7,17 @@ namespace HotelBookingBlazor.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-        [Required, MaxLength(25), RegularExpression(@"^[a-zA-Z]+$")]
+        [Required, MaxLength(25), RegularExpression(@"^[a-zA-Z]+$"), Unicode(false)]
         public required string FirstName { get; set; }
 
-        [MaxLength(25)]
+        [MaxLength(25), Unicode(false)]
         public string? LastName { get; set; }
 
+        [Required, MaxLength(8), Unicode(false)]
         public string? RoleName { get; set; }
+
+        [Required, MaxLength(15), RegularExpression(@"[0-9\+\(\)\s]")]
+        public string ContactNumber { get; set; }
     }
 
 }
---
+++ HotelBookingBlazor/Data/Migrations/20260320065606_AddContactNumber.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelBookingBlazor.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddContactNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RoleName",
+                table: "AspNetUsers",
+                type: "varchar(8)",
+                unicode: false,
+                maxLength: 8,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactNumber",
+                table: "AspNetUsers",
+                type: "varchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactNumber",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RoleName",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(8)",
+                oldUnicode: false,
+                oldMaxLength: 8);
+        }
+    }
+}
